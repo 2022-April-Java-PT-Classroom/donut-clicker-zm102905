5d509b58e0e8f9c8e5cbccd651e63e922093a8ba
zm102905 <zm102905@gmail.com>
2022-07-07 15:07:10 -0400

    finally finished

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,36 +1,76 @@
 class DonutMaker {
-
-    constructor() {
+//Constructor for Donut Maker.  
+    constructor(onDonutCountChanged, onAutoClickerPurchased, onMultiplierPurchased) {
         this.donutCount = 0;
         this.autoClickerCount = 0;
-        this.donutMultiplierCount=1;
+        this.donutMultiplierCount=0;
         this.donutMultiplierCost = 10;
         this.autoClickerCost = 100;
+//The next three are here to tell the browser when to update the "count" values in the DOM.
+        this.onDonutCountChanged = onDonutCountChanged;
+        this.onAutoClickerPurchased = onAutoClickerPurchased;
+        this.onMultiplierPurchased = onMultiplierPurchased;
+    }
+    //Click function. This changes the donut count. Calculation for multipliers was included to account for the increase in value of each click for each multiplier purchased. The onDonutCountChanged function, will update the donut count every time it changes. 
+    click() {
+        this.donutCount += Math.pow(1.2, this.donutMultiplierCount);
+    //Tells the browser to update donut count.
+        this.onDonutCountChanged();
     }
 
-    addToDonutCount() {
-        this.donutCount ++;
-    }
-
+    //Getters. Applied Math.round to the donut count, so a whole number will be returned. 
     getDonutCount() {
-        return this.donutCount;
+        return Math.round(this.donutCount);
     }
 
     getAutoClickerCount() {
         return this.autoClickerCount;
     }
 
-    addToAutoClickerCount() {
-        if (this. donutCount >= this.autoClickerCost){
-            this.donutCount -= this.authoClickerCost;
+    getDonutMultiplierCount() {
+        return this.donutMultiplierCount;
+    }
+
+    purchaseAutoClicker() {
+        if (this.donutCount >= this.autoClickerCost){
+            this.donutCount -= this.autoClickerCost;
+    //Needs to update donut count in the browser, because it changed when we subtracted the autoclicker cost. 
+            this.onDonutCountChanged();
             this.autoClickerCount ++;
-            this.autoClickerCost = Math.round(this.autoClickCost * 1.1);
+            this.autoClickerCost = (this.autoClickerCost * 1.1);
+    //Tells browser to update the autoclicker count.
+            this.onAutoClickerPurchased();    
+        }
+    }
+    //Clicks for every autoclicker in the count.
+    activateAutoClickers(){ 
+        for (let i=0; i < this.autoClickerCount; i++) {
+            this.click();
         }
     }
 
-    activateAutoClickers() {
-        this.donutCount = this.donutCount + this.autoClickerCount;
+    canBuyAutoClicker() {
+        return this.donutCount >= this.autoClickerCost;
     }
+
+    canBuyMultiplier() {
+        return this.donutCount >= this.donutMultiplierCost;
+    }
+
+    purchaseMultiplier() {
+        if (this. donutCount >= this.donutMultiplierCost){
+            this.donutCount -= this.donutMultiplierCost;
+            this.onDonutCountChanged();
+            this.donutMultiplierCount ++
+            this.donutMultiplierCost = (this.donutMultiplierCost * 1.1);
+            this.onMultiplierPurchased();
+            
+        }
+    }
+
 }
 
-export{DonutMaker};
+
+
+
+export default DonutMaker;
